1dcc8db118f79a22f4250d1d4b4b8378b49d29a3
Philip Hinchsliff <philip.hinchsliff@yahoo.com>
2021-09-12 20:58:44 +0300

    commit add the button functions

--- app.js
+++ app.js
@@ -2,28 +2,28 @@
 
 const countriesURL = 'https://restcountries.eu/rest/v2/all';
 
-const mainContainer = document.querySelector('.main-container');
+let mainContainer = document.querySelector('.main-container');
 const form = document.querySelector('.form');
+const mostPopBtn = document.querySelector('.most-populated');
+const sortAZBtn = document.querySelector('.sort-az');
+const sortZABtn = document.querySelector('.sort-za');
+const largestAreaBtn = document.querySelector('.largest-area');
 const input = document.getElementById('input');
 
 let countriesArr = [];
-
-
-
-
-
 
 // GET DATA FROM API
 async function getCountriesData() {
     const response = await fetch(countriesURL);
     const data = await response.json();
     showCountries(data);
+
 }
 
 //SHOW COUNTRIES 
 function showCountries(countries) {
     countries.forEach((country) => {
-        countriesArr.push(country)
+        countriesArr.push(country);
         let box = document.createElement('div');
         box.classList.add('box');
         if (country.area != null) {
@@ -60,6 +60,16 @@ function showCountries(countries) {
     })
 }
 
+//SORT THE ARRAY TEMPLATE FUNCTION
+function sortCountries(arr, type) {
+    const countries = [...arr]
+    const sortedCountries = countries.sort((a, b) => {
+        if (a[type] > b[type]) return -1
+        if (a[type] < b[type]) return 1
+        return 0
+    })
+    return sortedCountries
+}
 
 // FORMAT NUMBERS TO ADD COMMA EVERY 3 DIGITS
 function formatNumbers(number) {
@@ -79,9 +89,52 @@ form.addEventListener('submit', (e) => {
     input.value = '';
 })
 
+mostPopBtn.addEventListener('click', () => {
+    mainContainer.innerHTML = '';
+    let mostPopCountries = sortCountries(countriesArr, 'population');
+
+    const ids = mostPopCountries.map(o => o.name)
+    const filtered = mostPopCountries.filter(({ name }, index) => !ids.includes(name, index + 1))
+
+    showCountries(filtered)
+})
+
+sortAZBtn.addEventListener('click', () => {
+    mainContainer.innerHTML = '';
+    let sortedCountriesAZ = sortCountries(countriesArr, 'name');
+
+    const ids = sortedCountriesAZ.map(o => o.name)
+    const filtered = sortedCountriesAZ.filter(({ name }, index) => !ids.includes(name, index + 1)).reverse()
+
+    showCountries(filtered)
+})
+
+sortZABtn.addEventListener('click', () => {
+    mainContainer.innerHTML = '';
+    let sortedCountriesAZ = sortCountries(countriesArr, 'name');
+
+    const ids = sortedCountriesAZ.map(o => o.name)
+    const filtered = sortedCountriesAZ.filter(({ name }, index) => !ids.includes(name, index + 1))
+
+    showCountries(filtered)
+})
+
+largestAreaBtn.addEventListener('click', () => {
+    mainContainer.innerHTML = '';
+    let sortedCountriesAZ = sortCountries(countriesArr, 'area');
+
+    const ids = sortedCountriesAZ.map(o => o.name)
+    const filtered = sortedCountriesAZ.filter(({ name }, index) => !ids.includes(name, index + 1))
+
+    showCountries(filtered)
+})
+
 
 getCountriesData()
-    .then(res => {
-        countriesArr.push(res);
-    })
+
+
+// REPLACE THE Å IN ALAND ISLAND TO ENGLISH ALPHABET A
+
+// FINISH THE TEXT INPUT SEARCH
+
 
